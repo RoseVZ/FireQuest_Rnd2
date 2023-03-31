7535e8375eb036d54599919c9104c2346e0aa239
Muskan-Bansal <105799897+muskan-Bansal@users.noreply.github.com>
2023-04-01 00:13:02 +0530

    changees

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
+
+app.use(cors({ origin: 'http:localhost:3000' }));
 
 var admin = require('firebase-admin');
 
