9b42dbd129fef77e2d2f47df68a21a6afb5d1ec2
RoseVZ <priyarosev949@gmail.com>
2023-03-31 22:46:26 +0530

    Tables

---
+++ frontend/frontend2/src/Components/WasteTable.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NonRecyc from './tables/NonRecyc';
+import RecyTable from './tables/RecyTable';
+
+function wasteTable() {
+  return (
+      <div>
+          <RecyTable/>
+          <NonRecyc />
+          
+    </div>
+  )
+}
+
+export default wasteTable
